1ea01884511a4e8a9aa08674474231faf858c624
Ayan Bhuin <ayanbhuin@outlook.com>
2020-05-16 14:01:32 +0530

    Modified ProjectRequestData class

--- src/main/java/com/example/projecttracker/request/v1/ProjectRequestData.java
+++ src/main/java/com/example/projecttracker/request/v1/ProjectRequestData.java
@@ -1,14 +1,25 @@
 package com.example.projecttracker.request.v1;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 /**
  * add or modify request object for Project
  * @author Ayan Bhuin
  * @since April 29, 2020
  */
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
 public class ProjectRequestData {
+    @NotNull
+    @Size(min = 3, max = 250)
     private String name;
+
     private String description;
 }
