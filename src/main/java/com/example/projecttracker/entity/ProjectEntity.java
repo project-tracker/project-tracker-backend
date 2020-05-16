997aca65e3e976d447f0bdd3883d9c57a6c274f3
Ayan Bhuin <ayanbhuin@outlook.com>
2020-05-16 13:25:31 +0530

    modified ProjectEntity class

--- src/main/java/com/example/projecttracker/entity/ProjectEntity.java
+++ src/main/java/com/example/projecttracker/entity/ProjectEntity.java
@@ -1,5 +1,15 @@
 package com.example.projecttracker.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
 /**
  * Entity for Project
  *
@@ -7,5 +17,41 @@ package com.example.projecttracker.entity;
  * @since April 29, 2020
  */
 
-public class ProjectEntity {
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "project")
+public class ProjectEntity implements Serializable {
+
+    @Id
+    @SequenceGenerator(name = "project_seq", initialValue = 10000)
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "project_seq")
+    @Column
+    private int id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private String description;
+
+    @Column(nullable = false)
+    private boolean isDeleted;
+
+    @Column
+    private String createdBy;
+
+    @Column
+    @CreationTimestamp
+    @Temporal(value = TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Column
+    private String updatedBy;
+
+    @Column
+    @UpdateTimestamp
+    @Temporal(value = TemporalType.TIMESTAMP)
+    private Date updatedDate;
 }
